19629cc5d7c2c0646e9986569f8f4cfca00c2e85
RoscandDesign <59227392+Roscand@users.noreply.github.com>
2022-03-29 12:53:34 +0400

    [server] Implemented the main() method

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 const dotenv = require('dotenv').config();
+const Server = require('./server/Server');
+
+async function main() {
+  const port = process.env.PORT || 5000;
+  const mode = process.env.MODE;
+  const server = new Server(port, mode);
+  await server.listen();
+}
+
+main();
