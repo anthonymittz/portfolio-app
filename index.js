16c82503869b8a2f13de17b0bf2a7b49f8e4e3a0
RoscandDesign <59227392+Roscand@users.noreply.github.com>
2022-03-29 15:02:36 +0400

    [server] Added dev/prod servers initialization

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const Server = require('./server/Server');
 
 async function main() {
   const port = process.env.PORT || 5000;
-  const mode = process.env.MODE;
+  const mode = process.env.NODE_ENV;
   const server = new Server(port, mode);
-  await server.listen();
+  await server.start();
 }
 
 main();
--- server/Server.js
+++ server/Server.js
@@ -1,6 +1,6 @@
-const path = require('path');
-const { parse } = require('url');
 require('colors');
+const Router = require('./Router');
+const Middleware = require('./Middleware');
 const initNext = require('./modules/Next');
 const initExpress = require('./modules/Express');
 const createHttpsServer = require('./modules/Https');
@@ -9,21 +9,48 @@ class Server {
   constructor(port, mode) {
     this.port = port;
     this.mode = mode;
-    this.dev = this.mode === 'development';
+    this.dev = mode === 'development';
     this.host = this.dev ? 'localhost' : '';
+
     this.next = initNext(this.dev, this.host, this.port);
     this.express = initExpress();
     this.https = createHttpsServer(this.express, this.dev);
+
+    this.router = new Router(this.express, this.next);
+    this.middleware = new Middleware(this.express);
   }
 
-  async listen() {}
-
-  static log(message) {
-    console.log('[server]'.cyan, message);
+  async start() {
+    await this.next.prepare();
+    await this.router.init();
+    Server.log('Initialized', 'Router');
+    await this.middleware.init();
+    Server.log('Initialized', 'Middleware');
+    this.dev ? await this.startHttpsServer() : await this.startExpressServer();
   }
 
-  static error(message) {
-    console.error('[server]'.bgRed.white.bold, 'Error with the message:', `"${message}"`);
+  async startHttpsServer() {
+    await this.https.listen(this.port, err => this.callback(err, this.port));
+    Server.log('Https server is started');
+  }
+
+  async startExpressServer() {
+    await this.express.listen(this.port, err => this.callback(err, this.port));
+    Server.log('Express server is started');
+  }
+
+  /* Loggers */
+
+  callback(err, port) {
+    err ? Server.error(err.message) : Server.log(`Listening on port ${port}`);
+  }
+  static log(message, source) {
+    let signature = source ? `${source}`.cyan + ' -' : '-';
+    console.log('[server]'.cyan, signature, message);
+  }
+  static error(message, source) {
+    let signature = source ? `${source}`.red + ' -' : '-';
+    console.error('[server]'.bgRed.white.bold, signature, 'Error:', `"${message}"`);
   }
 }
 
